dafc81bd958043fa0a9c47a002ba3df223a6a49b
Nicholas Hubacek <Nicholas.Hubacek@gmail.com>
2023-12-05 13:32:58 -0600

    added auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
