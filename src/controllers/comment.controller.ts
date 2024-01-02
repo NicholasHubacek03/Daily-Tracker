5b0139767e3944d47513011102e5d8dc1263b3e8
Nicholas <Nicholas.Hubacek@gmail.com>
2024-01-02 13:04:40 -0600

    comment

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -11,5 +11,27 @@
       async getAllComments(): Promise<Comment[]> {
         return this.commentsService.findAll();
       }
+      @Get('id')
+      
     }
-    
+    //       @Get()
+    //   async getAllTasks(): Promise<Task[]> {
+    //     return this.tasksService.findAll();
+    //   }
+    //   @Get('id')
+    //   getTaskById(@Param('id') id: number): Task {
+    //     return this.tasksService.getTaskById(id);
+    //   }
+    //   @Post()
+    //   async createTask(@Body() taskData: ITaskDTO): Promise <Task>{
+    //     return Task.create({ firstName: taskData.title, lastName: taskData.description});
+    //   }
+    //   @Put(':id')
+    //   update(@Param('id') id: number, @Body() taskData: ITaskDTO){
+    //   return this.tasksService.updateTask(id, taskData);
+    //     }
+    //   @Delete('id')
+    //   remove(@Param('id') id: number){
+    //     return this.tasksService.remove(id);
+    //     }
+    // }
